2e0bb5c97052ae1794b4873d7913e4ee7d778c52
Yassine Nassibi <my.yassinenassibi@gmail.com>
2025-01-06 12:16:10 +0100

    mariaDB container in proccessing

---
+++ src/requirements/mariadb/script/mysql_dbConf.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+configure_zsh() {
+    echo "Configuring Zsh..."
+    chsh -s $(which zsh)
+    wget -q https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O ~/install.sh
+    echo "alias ohmyzsh-install='sh ~/install.sh'" >> ~/.zshrc
+}
+
+start_mariadb() {
+    echo "Starting MariaDB service..."
+    service mariadb start
+    while ! mysqladmin ping --silent; do sleep 1; done
+}
+
+configure_mariadb() {
+    echo "Configuring MariaDB..."
+    mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
+    mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \`${MYSQL_USER}\`@'%';"
+    mariadb -e "FLUSH PRIVILEGES;"
+}
+
+restart_mariadb() {
+    echo "Restarting MariaDB..."
+    mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+    mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
+}
+
+configure_zsh
+start_mariadb
+configure_mariadb
+restart_mariadb
