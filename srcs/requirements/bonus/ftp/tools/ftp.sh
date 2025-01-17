efa8a7898c798a316328803699f95417dcdbf4b5
ynassibi <ynassibi42@student.fr>
2025-01-17 18:59:11 +0100

    static running

--- srcs/requirements/bonus/ftp/tools/ftp.sh
+++ srcs/requirements/bonus/ftp/tools/ftp.sh
@@ -1,32 +1,12 @@
 #!/bin/sh
 
-mkdir -p /etc/vsftpd
-touch /etc/vsftpd.userlist
-
+useradd -m $UFTP
+echo "$UFTP:$PFTP" | chpasswd
+mkdir -p /home/$UFTP/ftp/upload
 mkdir -p /var/run/vsftpd/empty
+chown nobody:nogroup /home/$UFTP/ftp/upload
+chmod a-w /home/$UFTP/ftp
+chmod 755 /home/$UFTP/ftp/upload
+chmod 755 /var/run/vsftpd/empty 
 
-
-if [ ! -f "/etc/vsftpd/vsftpd.conf.bak" ]; then
-    echo "Initializing FTP server configuration..."
-
-    mkdir -p /var/www/html
-    cp /etc/vsftpd/vsftpd.conf /etc/vsftpd/vsftpd.conf.bak
-    mv /tmp/vsftpd.conf /etc/vsftpd/vsftpd.conf
-
-    if ! id "$FTP_USR" &> /dev/null; then
-        adduser "$FTP_USR" --disabled-password --gecos ""
-        echo "$FTP_USR:$FTP_PWD" | /usr/sbin/chpasswd
-    else
-        echo "User $FTP_USR already exists. Skipping user creation."
-    fi
-
-    chown -R "$FTP_USR:$FTP_USR" /var/www/html
-    chmod -R 755 /var/www/html
-
-    grep -qx "$FTP_USR" /etc/vsftpd.userlist || echo "$FTP_USR" >> /etc/vsftpd.userlist
-
-    echo "FTP server configuration initialized."
-fi
-
-echo "Starting FTP server on port 21..."
-exec /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
+vsftpd /etc/vsftpd.conf
--- srcs/requirements/bonus/webstatics/tools/web.sh
+++ srcs/requirements/bonus/webstatics/tools/web.sh
@@ -1,5 +1,13 @@
-#!bin/env/bash 
+#!/bin/bash 
 
-# This script is used to deploy the webstatics project on the server.
+# URL of the repository to clone
+REPO_URL="https://github.com/Nassibi107/templite-css-html-3.git"
 
-https://codeload.github.com/Nassibi107/templite-css-html-3/zip/refs/heads/main
+# Directory where the repository will be cloned
+DEST_DIR="/home/app/webTemplate"
+
+# Clone the repository using git
+echo "Cloning repository..."
+git clone $REPO_URL /home/app/webTemplate
+
+echo "Repository cloned to $DEST_DIR."
