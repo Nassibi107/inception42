b3f067f3d5b95185c39adcf8c371832ffb682204
ynassibi <ynassibi42@student.fr>
2025-01-26 17:43:11 +0100

    bulleyes add in adminer -_-

--- srcs/requirements/bonus/adminer/tools/setup.sh
+++ srcs/requirements/bonus/adminer/tools/setup.sh
@@ -1,12 +1,11 @@
+apt install php php-mysql wget -y 
 
-wget "http://www.adminer.org/latest.php" -O /var/www/html/adminer.php 
-chown -R www-data:www-data /var/www/html/adminer.php 
+wget "http://www.adminer.org/latest.php" -O /var/www/html/adminer.php
+chown -R www-data:www-data /var/www/html/adminer.php
 chmod 755 /var/www/html/adminer.php
 
-mv ./adminer-4.8.1.php /var/www/html/index.php
-
 cd /var/www/html
-adduser -u 82 -D -S -G www-data www-data
+
 rm -rf index.html
 
-php-fpm8 --nodaemonize
+php -S 0.0.0.0:600 /var/www/html/adminer.php
